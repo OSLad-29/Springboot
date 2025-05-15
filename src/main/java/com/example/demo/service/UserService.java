1ec5a8d81f71a9f63763a6ceacff1a6e154091fd
OSLad-29 <somafu@outlook.com>
2025-05-15 11:48:42 +0200

    Defined UserService interface with core operations

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,4 +1,9 @@
 package com.example.demo.service;
 
-public class UserService {
+public interface UserService {
+    String addUser(String name, String surname);
+
+    String removeUser(long id);
+
+    String getUser(long id);
 }
