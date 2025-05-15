273e9ba0b100567e8e87a1290f511a453cdb5dc8
OSLad-29 <somafu@outlook.com>
2025-05-15 10:14:33 +0200

    Added User model class

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,17 @@
 package com.example.demo.model;
 
 public class User {
+    private long id;
+    private String name;
+    private String surname;
+
+    public User(long id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
