d8680e1163e8a6740a66a4618f44d083289ce2af
OSLad-29 <somafu@outlook.com>
2025-05-15 12:09:15 +0200

    Created UserServiceImpl with dependency injection

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,4 +1,15 @@
 package com.example.demo.service;
 
-public class UserServiceImpl {
+import com.example.demo.repo.FakeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final FakeRepo fakeRepo;
+
+    @Autowired
+    public UserServiceImpl(FakeRepo fakeRepo) {
+        this.fakeRepo = fakeRepo;
+    }
 }
