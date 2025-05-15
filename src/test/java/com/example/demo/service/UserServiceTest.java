5c91140dc838f4b9ebae8a14b08bf158c4ab400b
OSLad-29 <somafu@outlook.com>
2025-05-15 14:41:18 +0200

    test: setup UserServiceTests

---
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTests {
+    @Test
+    void contextLoads() {
+    }
+}
