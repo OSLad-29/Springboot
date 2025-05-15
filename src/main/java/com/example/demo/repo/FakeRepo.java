3e9598e89e9fef329e60be96c7432cbfa5073952
OSLad-29 <somafu@outlook.com>
2025-05-15 11:34:29 +0200

    Implemented FakeRepo with in-memory storage

--- src/main/java/com/example/demo/repo/FakeRepo.java
+++ src/main/java/com/example/demo/repo/FakeRepo.java
@@ -1,4 +1,16 @@
 package com.example.demo.repo;
 
-public class FakeRepo {
+import com.example.demo.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FakeRepo implements FakeRepoInterface {
+    private User[] users = new User[10];
+    private int userCount = 0;
+
+    @Override
+    public String insertUser(long id, String name, String surname) {
+        users[userCount++] = new User(id, name, surname);
+        return name;
+    }
 }
